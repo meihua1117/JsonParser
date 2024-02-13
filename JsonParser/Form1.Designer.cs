864885d96630d0d7929aa3f9a8093fc12c3ebed4
jinjin <meihua_1117@outlook.com>
2024-02-13 17:04:58 +0900

    Add project files.

---
+++ JsonParser/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace JsonParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnParsing = new System.Windows.Forms.Button();
+            this.Btn_Check_File_Create = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileBtn
+            // 
+            this.fileBtn.Location = new System.Drawing.Point(20, 32);
+            this.fileBtn.Name = "fileBtn";
+            this.fileBtn.Size = new System.Drawing.Size(125, 23);
+            this.fileBtn.TabIndex = 0;
+            this.fileBtn.Text = "json파일 선택";
+            this.fileBtn.UseVisualStyleBackColor = true;
+            this.fileBtn.Click += new System.EventHandler(this.fileClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnParsing
+            // 
+            this.btnParsing.Location = new System.Drawing.Point(151, 32);
+            this.btnParsing.Name = "btnParsing";
+            this.btnParsing.Size = new System.Drawing.Size(125, 23);
+            this.btnParsing.TabIndex = 1;
+            this.btnParsing.Text = "파싱시작";
+            this.btnParsing.UseVisualStyleBackColor = true;
+            this.btnParsing.Click += new System.EventHandler(this.btnParsing_Click);
+            // 
+            // Btn_Check_File_Create
+            // 
+            this.Btn_Check_File_Create.Location = new System.Drawing.Point(20, 30);
+            this.Btn_Check_File_Create.Name = "Btn_Check_File_Create";
+            this.Btn_Check_File_Create.Size = new System.Drawing.Size(125, 23);
+            this.Btn_Check_File_Create.TabIndex = 2;
+            this.Btn_Check_File_Create.Text = "Check 파일만들기";
+            this.Btn_Check_File_Create.UseVisualStyleBackColor = true;
+            this.Btn_Check_File_Create.Click += new System.EventHandler(this.Btn_Check_File_Create_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.fileBtn);
+            this.groupBox1.Controls.Add(this.btnParsing);
+            this.groupBox1.Location = new System.Drawing.Point(25, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(376, 133);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Json 파싱하기";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Btn_Check_File_Create);
+            this.groupBox2.Location = new System.Drawing.Point(25, 183);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(376, 135);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " Check 파일만들기";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button fileBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnParsing;
+        private System.Windows.Forms.Button Btn_Check_File_Create;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ JsonParser/Form1.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace JsonParser
+{
+
+    public partial class Form1 : Form
+    {
+        string jsonFile = null;
+        string targetFile = null;
+        string checkBaseFile = null;
+        string checkResultFile = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void fileClick(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "json 파일 (*.json)|*.json|모든 파일 (*.*)|*.*"; // 허용할 파일 유형을 지정합니다.
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // 대화 상자가 열릴 기본 디렉토리를 지정합니다.
+
+                DialogResult result = openFileDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(openFileDialog.FileName))
+                {
+                    // 선택한 파일 경로를 사용하여 원하는 작업을 수행합니다.
+                    jsonFile = openFileDialog.FileName;
+                    targetFile = jsonFile.Replace(".json", ".xlsx");
+                }
+            }
+        }
+
+        private void btnParsing_Click(object sender, EventArgs e)
+        {
+            if (jsonFile == null)
+            {
+                MessageBox.Show("파싱할 json파일을 선택해주세요.");
+                return;
+            }
+            var index = 0;
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Add();
+            Excel.Worksheet worksheet = workbook.ActiveSheet;
+            // 예시로 선택한 파일 경로를 출력합니다.
+            using (StreamReader sr = new StreamReader(jsonFile))
+            {
+                string jsonContent;
+                // 파일의 끝까지 한 줄씩 읽어오기
+                while ((jsonContent = sr.ReadLine()) != null)
+                {
+                    JObject jsonObject = JObject.Parse(jsonContent);
+                    var tag_str = jsonObject["tags"];
+                    var ip = (string)jsonObject["ip_str"];
+                    bool is_honeypot = false;
+                    if (tag_str != null)
+                    {
+                        JArray tags = (JArray)tag_str;
+                        foreach (var item in tags)
+                        {
+                            string tag = (string)item;
+                            if (tag == "honeypot")
+                            {
+                                is_honeypot = true;
+                                break;
+                            }
+                        }
+                    }
+                    if(!is_honeypot)
+                    {
+                        var http_status = 200;
+                        var http_str = jsonObject["http"];
+                        if (http_str != null)
+                        {
+                            http_status = (int)jsonObject["http"]["status"];
+                        }
+                        if(http_status == 200)
+                        {
+                            index++;
+                            worksheet.Cells[index, 1] = ip;
+                            worksheet.Cells[index, 2] = (string)jsonObject["port"];
+                            var country_code = (string)jsonObject["location"]["country_code"];
+                            worksheet.Cells[index, 3] = country_code;
+                            var domains_str = jsonObject["domains"];
+                            if (domains_str != null)
+                            {
+                                JArray domains = (JArray)domains_str;
+                                worksheet.Cells[index, 4] = string.Join(",", domains);
+                            }
+                        }
+                    }
+                }
+            }
+            workbook.SaveAs(targetFile);
+            // Excel 관련 객체 해제
+            workbook.Close();
+            excelApp.Quit();
+            jsonFile = null;
+            MessageBox.Show("json파싱이 완료되었습니다.");
+        }
+        private void Btn_Check_File_Create_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "json 파일 (*.txt)|*.txt|모든 파일 (*.*)|*.*"; // 허용할 파일 유형을 지정합니다.
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // 대화 상자가 열릴 기본 디렉토리를 지정합니다.
+
+                DialogResult result = openFileDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(openFileDialog.FileName))
+                {
+                    // 선택한 파일 경로를 사용하여 원하는 작업을 수행합니다.
+                    checkBaseFile = openFileDialog.FileName;
+                    checkResultFile = checkBaseFile.Replace(".txt", ".chk");
+                    string base_content = null;
+                    using (StreamReader sr = new StreamReader(checkBaseFile))
+                    {
+                        using (StreamWriter sw = new StreamWriter(checkResultFile))
+                        {
+                            // 파일의 끝까지 한 줄씩 읽어오기
+                            while ((base_content = sr.ReadLine()) != null)
+                            {
+                                string[] contents = base_content.Split('\t');
+                                sw.WriteLine("set rport " + contents[1]);
+                                sw.WriteLine("check " + contents[0]);
+                            }
+                            MessageBox.Show("Check파일생성이 완료되었습니다.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
